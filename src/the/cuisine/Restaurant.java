1ce4e078742f6eddf2407be468db2fdc40ea593a
1894434 <1894434@E21036.CEGEPGIM.LCL>
2019-02-17 09:31:32 -0500

    setter method of restaurant class

--- src/the/cuisine/Restaurant.java
+++ src/the/cuisine/Restaurant.java
@@ -51,6 +51,15 @@ public class Restaurant {
      return this.website;
     }
     
-    
+    //setter method
+    public void r_Setter(int id,int r_contact,String r_name,String r_address,String r_email,String r_website)
+    {
+     this.r_id=id;
+     this.contact=r_contact;
+     this.name=r_name;
+     this.address=r_address;
+     this.email=r_email;
+     this.website=r_website;
+    }
 }
 

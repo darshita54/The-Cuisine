dbaa0b1545de37af720d2fc1c5d8c08250456bb3
1894434 <1894434@E21036.CEGEPGIM.LCL>
2019-02-17 09:25:17 -0500

    constructor of restaurant class

--- src/the/cuisine/Restaurant.java
+++ src/the/cuisine/Restaurant.java
@@ -14,5 +14,14 @@ public class Restaurant {
     int r_id,contact;
     String name,address,email,website;
     
-    
+    //constructor
+    public Restaurant(int id,int r_contact,String r_name,String r_address,String r_email,String r_website)
+    {
+     this.r_id=id;
+     this.contact=r_contact;
+     this.name=r_name;
+     this.address=r_address;
+     this.email=r_email;
+     this.website=r_website;
+    }
 }

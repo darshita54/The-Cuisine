5c54dc365e8fce59e5dd13f9cb3c5673ccda1762
1895268 <1895268@E21038.CEGEPGIM.LCL>
2019-02-17 09:00:15 -0500

    Class details added.

---
+++ src/the/cuisine/Employer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package the.cuisine;
+
+/**
+ *
+ * @author 1895268
+ */
+public class Employer extends Person {
+    double salary;
+}

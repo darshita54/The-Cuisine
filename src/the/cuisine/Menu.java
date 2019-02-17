5089184ce8df0656925966a64c559a432cefe835
1894434 <1894434@E21036.CEGEPGIM.LCL>
2019-02-17 09:14:14 -0500

    getter,setter and display method of menu class

--- src/the/cuisine/Menu.java
+++ src/the/cuisine/Menu.java
@@ -32,6 +32,41 @@ public class Menu {
     {
       return this.m_id;
     }
+    public String getType()
+    {
+     return this.type;
+    }
+    public String geCategory()
+    {
+     return this.category;
+    }
+    public Date getStartDate()
+    {
+     return this.startDate;
+    }
+    public Date getEndDate()
+    {
+     return this.endDate;
+    }
     
+    //setter method
+    public void mSetter(int id,String m_type,String m_category,Date s_date,Date e_date)
+    {
+      this.m_id=id;
+      this.type=m_type;
+      this.category=m_category;
+      this.startDate=s_date;
+      this.endDate=e_date;
+    }
+    
+    //display method
+    public void m_display()
+    {
+     System.out.println("Menu id: "+m_id);
+     System.out.println("Menu type: "+type);
+     System.out.println("Menu category: "+category);
+     System.out.println("Menu start date: "+startDate);
+     System.out.println("Menu end date: "+endDate);
+    }
     
 }

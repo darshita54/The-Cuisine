94b26a9d0c43a25e4e310cfd6ebb30c5d0d252c1
1895268 <1895268@E21038.CEGEPGIM.LCL>
2019-03-02 12:28:45 -0500

    added Order unit test

---
+++ test/OrderJUnitTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import the.cuisine.Person;
+
+/**
+ *
+ * @author 1895268
+ */
+public class OrderJUnitTest {
+    
+    public OrderJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testRestaurantID()
+    {
+     System.out.println("Test for Order ID");
+     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yy-MMM-dd");  
+        LocalDateTime now = LocalDateTime.now(); 
+        String date=dtf.format(now);
+        String fname="O";
+        String lname="3";
+        String expectedResult="19-Mar-02-O3";
+        String result=Person.createPersonID(date,fname,lname);
+        
+         assertEquals(expectedResult, result);
+        
+        
+     
+    }
+}
